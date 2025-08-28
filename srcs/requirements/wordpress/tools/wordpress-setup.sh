2d2954e32c416d7f116df9c72684236238d29137
oprosvir <ol7897@ya.ru>
2025-08-28 20:29:54 +0200

    debug(nginx+wordpress): remove unused files, fix file permissions and symlinks in setup script

--- srcs/requirements/wordpress/tools/wordpress-setup.sh
+++ srcs/requirements/wordpress/tools/wordpress-setup.sh
@@ -27,7 +27,7 @@ if [ ! -f wp-config.php ]; then
         --dbuser=${MYSQL_USER} \
         --dbpass=${MYSQL_PASSWORD} \
         --dbhost=${DB_HOST} \
-        --skip-check # to avoid connection check in a containerized environment
+        --skip-check
     
     # install WP with custom settings
     # admin user
@@ -37,7 +37,7 @@ if [ ! -f wp-config.php ]; then
         --admin_user=${WP_ADMIN_USER} \
         --admin_password=${WP_ADMIN_PASSWORD} \
         --admin_email=${WP_ADMIN_EMAIL} \
-        --skip-email # to avoid sending email in a containerized environment
+        --skip-email
     
     # create a secondary user with editor role
     wp user create ${WP_USER} ${WP_USER_EMAIL} \
@@ -45,10 +45,20 @@ if [ ! -f wp-config.php ]; then
         --role=editor \
         --allow-root
     
-    echo "✅ WordPress installed and configured successfully!"
+    # delete default plugins and themes
+    wp plugin delete akismet hello --allow-root
+
+    # set permalink structure to "Post name"
+    wp rewrite structure '/%postname%/' --hard --allow-root
+    wp rewrite flush --hard --allow-root
+
+    # set the correct ownership and permissions
+    chown -R www-data:www-data /var/www/html
+
+    echo "WordPress installed and configured successfully!"
     
 else
-    echo "✅ WordPress already installed, skipping setup..."
+    echo "WordPress already installed, skipping setup..."
 fi
 
 echo "Starting PHP-FPM..."
